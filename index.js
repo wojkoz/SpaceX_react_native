f035c26dc66cacc95f443c1079c3bd0ffe32f9ea
TheEnter3 <wkoziol3ib@gmail.com>
2020-03-10 17:12:53 +0100

    added bottom bar

--- index.js
+++ index.js
@@ -1,9 +1,80 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import App from './App';
-import {name as appName} from './app.json';
+import TeslaCar from './screens/TeslaCar';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('Events', () => App);
+Navigation.registerComponent('TeslaCar', () => TeslaCar);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      bottomTabs: {
+        children: [
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'Events',
+                    passProps: {
+                      title: 'Events',
+                    },
+                  },
+                },
+              ],
+              options: {
+                bottomTab: {
+                  text: 'Events',
+                  testID: 'FIRST_TAB_BAR_BUTTON',
+                },
+              },
+            },
+          },
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'TeslaCar',
+                    passProps: {
+                      title: 'TeslaCar',
+                    },
+                  },
+                },
+              ],
+              options: {
+                bottomTab: {
+                  text: 'Tesla Car',
+                  testID: 'FIRST_TAB_BAR_BUTTON2',
+                },
+              },
+            },
+          },
+          //nastepny widok
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'TeslaCar',
+                    passProps: {
+                      title: 'TeslaCar',
+                    },
+                  },
+                },
+              ],
+              options: {
+                bottomTab: {
+                  text: 'Tesla Car',
+                  testID: 'FIRST_TAB_BAR_BUTTON2',
+                },
+              },
+            },
+          },
+          //koniec
+        ],
+      },
+    },
+  });
+});
---
+++ screens/TeslaCar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const TeslaCar = () => {
+  return <Text>Tesla Car info</Text>;
+};
+
+export default TeslaCar;
